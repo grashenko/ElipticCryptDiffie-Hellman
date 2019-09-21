0bb33fed2f4685cd1562a7bb85f16c89b079ef7c
Alexander Grashenko <grashenko1410@mail.ru>
2019-09-21 23:35:19 +0300

    ElipticCrypt Project

---
+++ ElipticCrypt/DSGost.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ElipticCrypt
+{
+    public class DSGost
+    {
+        private BigInteger p = new BigInteger();
+        private BigInteger a = new BigInteger();
+        private BigInteger b = new BigInteger();
+        private BigInteger n = new BigInteger();
+        private byte[] xG;
+        private ECPoint G = new ECPoint();
+        
+        public DSGost(BigInteger p, BigInteger a, BigInteger b, BigInteger n, ECPoint G)
+        {
+            this.a = a;
+            this.b = b;
+            this.n = n;
+            this.p = p;
+            this.G = G;
+        }
+
+
+        //С помощью секретного ключа d вычисляем точку Q=d*G, это и будет наш публичный ключ
+        public ECPoint GenPublicKey(BigInteger d)
+        {
+            ECPoint Q = ECPoint.multiply(d, G);
+            return Q;
+        }
+
+
+        //функция вычисления квадратоного корня по модулю простого числа q
+      
+        //подписываем сообщение
+        public string SingGen(byte[] h, BigInteger d)
+        {
+            BigInteger alpha = new BigInteger(h);
+            BigInteger e = alpha % n;
+            if (e == 0)
+                e = 1;
+            BigInteger k = new BigInteger();
+            ECPoint C=new ECPoint();
+            BigInteger r=new BigInteger();
+            BigInteger s = new BigInteger();
+            do
+            {
+                do
+                {
+                    k.genRandomBits(n.bitCount(), new Random());
+                } while ((k < 0) || (k > n));
+                C = ECPoint.multiply(k, G);
+                r = C.x % n;
+                s = ((r * d) + (k * e)) % n;
+            } while ((r == 0)||(s==0));
+            string Rvector = padding(r.ToHexString(),n.bitCount()/4);
+            string Svector = padding(s.ToHexString(), n.bitCount() / 4);
+            return Rvector + Svector;
+        }
+
+        //проверяем подпись 
+        public bool SingVer(byte[] H, string sing, ECPoint Q)
+        {
+            string Rvector = sing.Substring(0, n.bitCount() / 4);
+            string Svector = sing.Substring(n.bitCount() / 4, n.bitCount() / 4);
+            BigInteger r = new BigInteger(Rvector, 16);
+            BigInteger s = new BigInteger(Svector, 16);
+            if ((r < 1) || (r > (n - 1)) || (s < 1) || (s > (n - 1)))
+                return false;
+            BigInteger alpha = new BigInteger(H);
+            BigInteger e = alpha % n;
+            if (e == 0)
+                e = 1;
+            BigInteger v = e.modInverse(n);
+            BigInteger z1 = (s * v) % n;
+            BigInteger z2 = n + ((-(r * v)) % n);
+            ECPoint A = ECPoint.multiply(z1, G);
+            ECPoint B = ECPoint.multiply(z2, Q);
+            ECPoint C = A + B;
+            BigInteger R = C.x % n;
+            if (R == r)
+                return true;
+            else
+                return false;
+        }
+
+        //дополняем подпись нулями слева до длины n, где n - длина модуля в битах
+        private string padding(string input, int size)
+        {
+            if (input.Length < size)
+            {
+                do
+                {
+                    input = "0" + input;
+                } while (input.Length < size);
+            }
+            return input;
+        }
+    }
+}
---
+++ ElipticCrypt/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ElipticCrypt
+{
+
+    class Program
+    {
+        public static string p = "6277101735386680763835789423207666416083908700390324961279"; //Модуль кривой
+
+        public static long a = -3;// Параметр a уравнения
+        public static string b = "2455155546008943817740293915197451784769108058161191238065";// Параметр b уравнения
+
+        public static string Xg = "602046282375688656758213480587526111916698976636884684818";//Координаты точки 
+        public static string Yg = "174050332293622031404857552280219410364023488927386650641";
+
+        public static string n = "6277101735386680763835789423176059013767194773182842284081";// Порядок точки
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите случайное число q сторна A:");
+            BigInteger ka = new BigInteger(Console.ReadLine(), 10);
+            var pointG = new ECPoint();
+            pointG.a = new BigInteger(a);
+            pointG.b = new BigInteger(b, 10);
+            pointG.x = new BigInteger(Xg, 10);
+            pointG.y = new BigInteger(Yg, 10);
+            pointG.FieldChar = new BigInteger(p, 10);
+
+            var Ya =  ECPoint.multiply(ka, pointG);
+
+            Console.WriteLine("Введите случайное число q сторна B:");
+            BigInteger kb = new BigInteger(Console.ReadLine(), 10);
+            var Yb = ECPoint.multiply(kb, pointG);
+
+            var Ka = ECPoint.multiply(ka, Yb);
+            var Kb = ECPoint.multiply(kb, Ya);
+
+            Console.WriteLine("Ключ K у стороны A: " + " X: " + Ka.x + " Y: " + Ka.y);
+            Console.WriteLine("Ключ K у стороны B: " + " X: " + Kb.x + " Y: " + Kb.y);
+            Console.ReadLine();
+            Console.WriteLine("Цифровая подпись ГОСТ ");
+
+            Console.WriteLine("Введите сообщение: ");
+            string message = Console.ReadLine();
+            GOST hash = new GOST(256);
+            byte[] H = hash.GetHash(Encoding.Default.GetBytes(message));
+            var gost = new DSGost(new BigInteger(p, 10), new BigInteger(a), new BigInteger(b, 10), new BigInteger(n, 10), pointG);
+
+            ECPoint Q = gost.GenPublicKey(kb);
+            var podpis = gost.SingGen
+                (H, kb);
+
+            Console.WriteLine("Подпись: " + podpis);
+            var isVerivicated = gost.SingVer(H, podpis, Q);
+            if (isVerivicated)
+            {
+                Console.WriteLine("Подпись верна");
+            }
+            else
+            {
+                Console.WriteLine("Подпись не верна");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
